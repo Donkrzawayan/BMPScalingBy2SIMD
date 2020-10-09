de4f44df84cb1de1f9fbcd6d56bfdce87669d5ed
Donkrzawayan <43960303+Donkrzawayan@users.noreply.github.com>
2020-10-09 12:25:00 +0200

    adds working C and masm dll

--- BMPScalingBy2SIMD/main.cpp
+++ BMPScalingBy2SIMD/main.cpp
@@ -1,29 +1,21 @@
-﻿// BMPScalingBy2SIMD.cpp : Ten plik zawiera funkcję „main”. W nim rozpoczyna się i kończy wykonywanie programu.
-//
-
+﻿//
 #include <iostream>
 #include <Windows.h>
 
-typedef int(_fastcall *MyProc1)(int, int);
+//typedef int(_fastcall *MyProc1)(int, int);
+typedef int(_fastcall *MyProc1)();
 
 int main()
 {
-	HINSTANCE dllHandle = NULL;
-	dllHandle = LoadLibrary(L"JAAsm.dll");
+	/*HINSTANCE dllHandle = NULL;
+	dllHandle = LoadLibrary(L"DllMasm.dll");
 	MyProc1 procedura = (MyProc1)GetProcAddress(dllHandle, "MyProc1");
 	int x = 5, y = 7;
-	int retVal = procedura(x, y);
+	int retVal = procedura(x, y);*/
+	HINSTANCE dllHandle = NULL;
+	dllHandle = LoadLibrary(L"DllC.dll");
+	MyProc1 fun = (MyProc1)GetProcAddress(dllHandle, "fun");
+	int i = fun();
 
-    std::cout << "Hello World!\n";
+	std::cout << "Hello World!\n" << i;
 }
-
-// Uruchomienie programu: Ctrl + F5 lub menu Debugowanie > Uruchom bez debugowania
-// Debugowanie programu: F5 lub menu Debugowanie > Rozpocznij debugowanie
-
-// Porady dotyczące rozpoczynania pracy:
-//   1. Użyj okna Eksploratora rozwiązań, aby dodać pliki i zarządzać nimi
-//   2. Użyj okna programu Team Explorer, aby nawiązać połączenie z kontrolą źródła
-//   3. Użyj okna Dane wyjściowe, aby sprawdzić dane wyjściowe kompilacji i inne komunikaty
-//   4. Użyj okna Lista błędów, aby zobaczyć błędy
-//   5. Wybierz pozycję Projekt > Dodaj nowy element, aby utworzyć nowe pliki kodu, lub wybierz pozycję Projekt > Dodaj istniejący element, aby dodać istniejące pliku kodu do projektu
-//   6. Aby w przyszłości ponownie otworzyć ten projekt, przejdź do pozycji Plik > Otwórz > Projekt i wybierz plik sln
---
+++ DllC/DllC.c
@@ -0,0 +1,5 @@
+#include "pch.h"
+
+int fun(void) {
+	return 21;
+}
