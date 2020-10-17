1739fdd81f3534d8224e6ab36d0383be9236ca61
Donkrzawayan <43960303+Donkrzawayan@users.noreply.github.com>
2020-10-17 15:48:48 +0200

    time measurement and setting the number of threads

--- BMPScalingBy2SIMD/main.cpp
+++ BMPScalingBy2SIMD/main.cpp
@@ -1,11 +1,12 @@
 ﻿//
-//#include <iostream>
+#include <iostream>
 #include <Windows.h>
 #include <cstring> //strcmp
 #include <sstream>
 #include "bmp.hpp"
 #include <thread>
 #include <queue>
+#include <chrono>
 
 //typedef uint8_t *(_fastcall *MyProc1)(uint8_t *, uint8_t *, int32_t, int32_t);
 typedef uint8_t *(_fastcall *MyProc1)(uint8_t *, uint8_t *, int32_t, unsigned);
@@ -21,21 +22,21 @@ void scaler(uint8_t *destination, uint8_t *source, int32_t width, unsigned size)
 	fun(destination, source, width, size);
 }
 
-void multithreating(const unsigned N, uint8_t *destination, uint8_t *source, const int32_t width, const int32_t height) {
-	const int32_t subpixelWidth = 3 * width;
+void multithreating(const unsigned N, uint8_t *dest, uint8_t *src, const int32_t width, const int32_t height) {
+	const int32_t subpxWidth = 3 * width;
 	const int32_t rowsPerThread = height / N;
 	const int32_t firstLoop = height % N;
 	std::queue<std::thread> threads;
 
 	//rowsPerThread+1
-	int32_t size = subpixelWidth * rowsPerThread + subpixelWidth;
-	for (int i = 0; i < firstLoop; ++i, destination += 4 * size, source += size)
-		threads.push(std::move(std::thread(scaler, destination, source, subpixelWidth, size)));
+	int32_t size = subpxWidth * rowsPerThread + subpxWidth;
+	for (int i = 0; i < firstLoop; ++i, dest += 4 * size, src += size)
+		threads.push(std::move(std::thread(scaler, dest, src, subpxWidth, size)));
 
 	//rowsPerThread
-	size = subpixelWidth * rowsPerThread;
-	for (unsigned i = 0; i < N - firstLoop; ++i, destination += 4 * size, source += size)
-		threads.push(std::move(std::thread(scaler, destination, source, subpixelWidth, size)));
+	size = subpxWidth * rowsPerThread;
+	for (unsigned i = 0; i < N - firstLoop; ++i, dest += 4 * size, src += size)
+		threads.push(std::move(std::thread(scaler, dest, src, subpxWidth, size)));
 
 	while (!threads.empty()) {
 		threads.front().join();
@@ -49,28 +50,42 @@ void initializeArray(uint8_t *data, int32_t size) {
 		data[i] = static_cast<uint8_t>(2);
 }
 
-int main(int argc, char *argv[])
+//template <typename ToDuration= std::chrono::milliseconds>
+//class Stoper {
+//
+//};
+
+int main(const int argc, char *argv[])
 {
 	const char *sourceName = "";
 	const char *destName = "";
-	unsigned thread = 0U;
+	unsigned thread = std::thread::hardware_concurrency() ? std::thread::hardware_concurrency() : 1U;
 	for (int i = 1; i < argc; ++i) {
 		if (!strcmp("-s", argv[i]))
 			sourceName = argv[++i];
 		else if (!strcmp("-d", argv[i]))
 			destName = argv[++i];
-		else if (!strcmp("-t", argv[i])) {
+		else if (!strcmp("-t", argv[i]) && strcmp("0", argv[++i])) {//if user choose 0 (default) set yourself
 			std::stringstream number;
-			number << argv[++i];
+			number << argv[i];
 			number >> thread;
 		}
 	}
+
+	std::cout << "Size of: " << sizeof(uint8_t *) << "\n";
 
 	BMP source(sourceName);
 	BMP dest(source, 2 * source.width, 2 * source.height);
 	//initializeArray(dest.data, dest.size);
 
+
+	auto start = std::chrono::high_resolution_clock::now();
+
 	multithreating(thread, dest.data, source.data, source.width, source.height);
+
+	auto stop = std::chrono::high_resolution_clock::now();
+	auto duration = std::chrono::duration_cast<std::chrono::microseconds>(stop - start);
+	std::cout << duration.count() << "\n";
 
 	dest.write(destName);
 }
--- DllC/DllC.c
+++ DllC/DllC.c
@@ -1,9 +1,11 @@
 #include "pch.h"
 #include <stdint.h> /* int32_t, uint8_t */
 #include <string.h> /* memcpy */
+#include <immintrin.h>
 
-uint8_t *fun(uint8_t *Dest, uint8_t *Source, int32_t sourceWidth, unsigned size) {
+uint8_t *fun(uint8_t *Destination, uint8_t *Source, int32_t sourceWidth, unsigned size) {
 	int32_t destWidth = 2 * sourceWidth;
+	uint8_t *Dest = Destination;
 	unsigned i;
 	for (i = 0; i < size; i += 3) {
 		if (!(i % sourceWidth) && i)
@@ -11,14 +13,17 @@ uint8_t *fun(uint8_t *Dest, uint8_t *Source, int32_t sourceWidth, unsigned size)
 
 		uint8_t *tempSource = Source + i;
 		uint8_t *tempDest = Dest + 2 * i;
-		uint8_t *downTempDest = tempDest + destWidth;
+		//uint8_t *underTempDest = tempDest + destWidth;
 		
 		/* 3 - one pixel, three subpixels */
 		memcpy(tempDest, tempSource, 3 * sizeof(uint8_t));
 		memcpy(tempDest + 3, tempSource, 3 * sizeof(uint8_t));
-		memcpy(downTempDest, tempSource, 3 * sizeof(uint8_t));
-		memcpy(downTempDest + 3, tempSource, 3 * sizeof(uint8_t));
+		//memcpy(underTempDest, tempSource, 3 * sizeof(uint8_t));
+		//memcpy(underTempDest + 3, tempSource, 3 * sizeof(uint8_t));
 	}
+	int32_t rows = size / sourceWidth;
+	for (int32_t i = 0; i < rows; ++i)
+		memcpy(&Destination[i * 2 * destWidth + destWidth], &Destination[i * 2 * destWidth], destWidth * sizeof(uint8_t));
 
-	return Dest;
+	return Destination;
 }
